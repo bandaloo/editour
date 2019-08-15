6847141c7e48ed2c4bc3c3c9b879761945f6bb2c
Joseph Petitti <jppetitti@wpi.edu>
2019-08-15 13:11:21 +0900

    add upload functionality

--- backend/index.js
+++ backend/index.js
@@ -1,36 +1,106 @@
 const express = require('express');
 const formidable = require('formidable');
+// const Zip = require('adm-zip');
+const archiver = require('archiver');
 const fs = require('fs');
-const jsZip = require('jszip');
 const app = express();
 
 const toursLoc = __dirname + '/tours/'; // TODO maybe change this?
 const tempLoc = __dirname + '/temp/'; // TODO maybe change this?
 
+// serve index
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
   // TODO serve the actual static front-end content
 });
 
+// static directory
+app.use(express.static(__dirname + '/static'));
 
 // endpoint for file uploads
 app.post('/upload', (req, res) => {
+  const tempDirPath = tempLoc + randName(10) + '/';
   const form = new formidable.IncomingForm();
+
   form.parse(req, (err, fields, files) => {
-    res.write(JSON.stringify(
-        {'status': 201, 'message': 'Received!'}
-    ));
-    console.log('Completed\n');
-    res.end();
+    // send errors back to client
+    if (err) {
+      res.status(500).send(JSON.stringify(
+          {'status': 500, 'message': err}
+      ));
+      return;
+    }
+    // check for missing or invalid 'name' field from client
+    if (typeof fields.name !== 'string') {
+      res.status(400).send(JSON.stringify(
+          {'status': 400, 'message': 'Missing or invalid name field'}
+      ));
+      return;
+    }
+
+    // create a file to stream archive data to
+    console.log('zipping and moving...');
+    const zipName = fields.name + '-' + new Date().valueOf() + '.zip';
+    const output = fs.createWriteStream(toursLoc + zipName);
+    const archive = archiver('zip');
+
+    // listen for all archive data to be written
+    // 'close' event is fired only when a file descriptor is involved
+    output.on('close', function() {
+      console.log('Zip written. ' + archive.pointer() + ' total bytes\n');
+      // send successful message to client
+      res.status(201).send(JSON.stringify(
+          {'status': 201, 'message': 'Received!'}
+      ));
+    });
+
+    // This event is fired when the data source is drained no matter what was
+    // the data source. It is not part of this library but rather from the
+    // NodeJS Stream API.
+    // @see: https://nodejs.org/api/stream.html#stream_event_end
+    output.on('end', function() {
+      console.log('Data has been drained');
+    });
+
+    // good practice to catch warnings (e.g. stat failures and other
+    // non-blocking errors)
+    archive.on('warning', function(err) {
+      if (err.code === 'ENOENT') {
+        // log warning
+        console.error(err);
+      } else {
+        // send errors back to client
+        res.status(500).send(JSON.stringify(
+            {'status': 500, 'message': err}
+        ));
+        return;
+      }
+    });
+
+    // good practice to catch this error explicitly
+    archive.on('error', function(err) {
+      // send errors back to client
+      res.status(500).send(JSON.stringify(
+          {'status': 500, 'message': err}
+      ));
+      return;
+    });
+
+    // pipe archive data to the file
+    archive.pipe(output);
+
+    // add the temp directory to the archive
+    archive.directory(tempDirPath, false);
+    // finalize to say we're done appending files
+    archive.finalize();
+
+    console.log('Completed');
   });
 
   // log progress
   form.on('progress', (bytesReceived, bytesExpected) => {
     const percent= (bytesReceived / bytesExpected) * 100;
     console.log(percent.toFixed(2) + '% complete...');
-    res.write(JSON.stringify(
-        {'status': 202, 'percentComplete': percent.toFixed(2)}
-    ));
   });
 
   // log errors
@@ -39,13 +109,42 @@ app.post('/upload', (req, res) => {
     res.status(500).send(JSON.stringify(
         {'status': 500, 'error': err}
     ));
+    return;
   });
 
   // save files to temp location initially
   form.on('fileBegin', (name, file) => {
-    file.path = tempLoc + file.name;
+    try {
+      fs.mkdirSync(tempDirPath, {recursive: true});
+    } catch (err) {
+      if (err.code !== 'EEXIST') {
+        res.status(500).send(JSON.stringify(
+            {'status': 500, 'message': 'Server failed to save files'}
+        ));
+        return;
+      } else {
+        // TODO make this generate a new name and try again on EEXIST
+      }
+    }
+    file.path = tempDirPath + file.name;
   });
 });
+
+
+/**
+ * returns a string of random numbers and letters, n characters long
+ * @param {number} n length of the random string
+ * @return {string} a random string
+ */
+const randName = (n) => {
+  const l = '1234567890abcdefghijklmnopqrstuvwxyz';
+  let out = '';
+  for (let i = 0; i < n; ++i) {
+    out += l[Math.floor(Math.random() * 36)];
+  }
+  return out;
+};
+
 
 app.listen(3000, () => {
   console.log('Now listening on port 3000...');
