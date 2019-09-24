a4e57df8691594c11039001addbc26d4a0bf88c4
Joseph Petitti <jppetitti@wpi.edu>
2019-09-24 12:09:43 +0900

    add autostart forever script

---
+++ autostart.sh
@@ -0,0 +1,32 @@
+#! /bin/bash
+
+# change this
+# EDITOUR_PATH="/home/username/editour"
+EDITOUR_PATH="/home/joe/Documents/editour"
+
+printUsage() {
+	echo "autostart.sh usage:"
+	echo -e "  autostart.sh start\tstarts the node daemon"
+	echo -e "  autostart.sh stop\tstops the node daemon"
+}
+
+if [ $# -lt 1 ]
+then
+	printUsage
+	exit 1
+fi
+
+if [ $1 == "start" ]
+then
+	echo "autostart.sh starting..."
+	"./node_modules/.bin/forever" start -a -l $EDITOUR_PATH/logs/forever.log -o $EDITOUR_PATH/logs/editour.log -e $EDITOUR_PATH/logs/error.log --sourceDir $EDITOUR_PATH index.js
+	exit 0
+elif [ $1 == "stop" ]
+then
+	echo "autostart.sh stopping..."
+	"./node_modules/.bin/forever" stop $EDITOUR_PATH/index.js
+	exit 0
+else
+	printUsage
+	exit 1
+fi
